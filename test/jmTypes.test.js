956c96c6a692a051bbb9794a2ae85a489bd09f54
darkcris1 <sircnujnuj@gmail.com>
2021-05-21 12:09:56 +0800

    chore: more testing

--- test/jmTypes.test.js
+++ test/jmTypes.test.js
@@ -1,91 +1,83 @@
-import jm from '../dist/jm.cjs'
-import { TYPE_KEY, isFunction, isRegExp } from '../src/is'
+import jm from "../src/index";
+import { TYPE_KEY, isFunction, isRegExp, isJMType } from "../src/is";
 
 function getInitialOptions(type) {
-  if (type === 'sameAs')
-    return {
-      [TYPE_KEY]: true,
-      config: {
-        sameAs: '',
-      },
-    }
-
   return {
     [TYPE_KEY]: true,
-    message: {},
+    messages: {},
     config: {
-      ...(type ? { type } : ''),
-      required: true,
+      ...(type ? { type } : ""),
+      required: !(type === "any"),
     },
-  }
+    allow: undefined,
+    label: undefined,
+  };
 }
 
-describe('json-msg types', () => {
-  it('check all the initial options', () => {
-    expect(jm.str({ min: 4 })).toMatchObject(getInitialOptions('string'))
-    expect(jm.array()).toMatchObject(getInitialOptions('array'))
-    expect(jm.num()).toMatchObject(getInitialOptions('number'))
-    expect(jm.sameAs()).toMatchObject(getInitialOptions('sameAs'))
-    expect(jm.bool()).toMatchObject(getInitialOptions('boolean'))
-    expect(jm.any()).toMatchObject(getInitialOptions())
-  })
-  it('jm.str()', () => {
-    const min = 5
-    const max = 5
-    const label = 'user'
-    const test = jm.str({ min, max, label, email: true, alphanum: true })
+describe("json-msg types", () => {
+  const min = 5;
+  const max = 5;
+  it("check all the initial options", () => {
+    expect(jm.str({ min: 4 })).toMatchObject(getInitialOptions("string"));
+    expect(jm.array()).toMatchObject(getInitialOptions("array"));
+    expect(jm.num()).toMatchObject(getInitialOptions("number"));
+    expect(jm.sameAs()).toMatchObject(getInitialOptions("sameas"));
+    expect(jm.bool()).toMatchObject(getInitialOptions("boolean"));
+    expect(jm.any()).toMatchObject(getInitialOptions("any"));
+  });
+  it("jm.str()", () => {
+    const label = "user";
+    const test = jm.str({ min, max, label, email: true, alphanum: true });
     expect(test).toMatchObject({
       config: { min, max },
       label,
-    })
-    expect(isFunction(test.config.email)).toBe(true)
-    expect(isRegExp(test.config.alphanum)).toBe(true)
-  })
-  it('jm.num()', () => {
-    const min = 5
-    const max = 5
-    const label = 'Pin'
-    const test = jm.num({ min, max, label, integer: true, float: true })
+    });
+    expect(test.config.email).toBe(true);
+    expect(test.config.alphanum).toBe(true);
+  });
+  it("jm.num()", () => {
+    const min = 5;
+    const max = 5;
+    const label = "Pin";
+    const test = jm.num({ min, max, label, integer: true, float: true });
     expect(test).toMatchObject({
       config: { min, max },
       label,
-    })
-    expect(isFunction(test.config.integer)).toBe(true)
-    expect(isFunction(test.config.float)).toBe(true)
-  })
-  it('jm.array()', () => {
-    const min = 5
-    const max = 5
-    const label = 'Items'
-    const test = jm.array({ min, max, label, items: jm.num() })
+    });
+    expect(test.config.integer).toBe(true);
+    expect(test.config.float).toBe(true);
+  });
+  it("jm.array()", () => {
+    const label = "Items";
+    const test = jm.array({ min, max, label, items: jm.num() });
     expect(test).toMatchObject({
       config: { min, max },
       label,
-    })
-    expect(isFunction(test.config.items)).toBe(true)
-  })
-  it('jm.bool()', () => {
-    const label = 'isGold'
-    const required = false
-    const test = jm.array({ required, label })
+    });
+    expect(isJMType(test.config.items)).toBe(true);
+  });
+  it("jm.bool()", () => {
+    const label = "isGold";
+    const required = false;
+    const test = jm.array({ required, label });
     expect(test).toMatchObject({
       config: { required },
       label,
-    })
-  })
-  it('jm.sameAs()', () => {
-    const label = 'confirm'
-    const test = jm.sameAs('pass', { label })
+    });
+  });
+  it("jm.sameAs()", () => {
+    const label = "confirm";
+    const test = jm.sameAs("pass", { label });
     expect(test).toMatchObject({
-      config: { sameAs: 'pass' },
+      config: { sameAs: "pass" },
       label,
-    })
-  })
-  it('jm.any()', () => {
-    const label = 'confirm'
-    const test = jm.any({ label })
+    });
+  });
+  it("jm.any()", () => {
+    const label = "confirm";
+    const test = jm.any({ label });
     expect(test).toMatchObject({
       label,
-    })
-  })
-})
+    });
+  });
+});
--- test/jmValidate.test.js
+++ test/jmValidate.test.js
@@ -1,79 +1,106 @@
-import jm from '../dist/jm.cjs'
+import jm from "../src/index";
+import { isJmArrayMsg } from "../src/is";
 
 const testSchema = {
   user: jm.str({ min: 5, max: 50, alphanum: true }),
   pass: jm.str({ min: 4 }),
-  confirm_pass: jm.sameAs('pass'),
+  confirm_pass: jm.sameAs("pass"),
   tags: jm.array({ min: 1, items: jm.str() }),
-  pin: jm.num({ digit: 4, integer: true, label: 'PIN' }),
+  pin: jm.num({ digit: 4, integer: true, label: "PIN" }),
   email: jm.str({ email: true }),
   isGold: jm.bool({ allow: [null] }),
   isAdmin: jm.bool(),
-}
+};
 const testData = {
-  user: 'user123',
-  pass: '12345',
-  confirm_pass: '12345',
-  tags: ['asdasd', 'asdasd'],
+  user: "user123",
+  pass: "12345",
+  confirm_pass: "12345",
+  tags: ["asdasd", "asdasd"],
   pin: 1234,
-  email: 'sircnujnuj@gmail.com',
+  email: "sircnujnuj@gmail.com",
   isGold: null,
   isAdmin: true,
-}
+};
+const arraySchema = jm.array({ items: [{ name: jm.str({ min: 5 }) }] });
+const arrayData = [{ name: 2 }];
 
-describe('Check the validation function', () => {
-  it('jm.validate() with no errors', () => {
-    const test = jm.validate(testData, testSchema)
-    expect(test).toBeNull()
-  })
-  it('jm.validate() with abortEarly: false', () => {
-    const cloneData = { ...testData }
-    cloneData.user = 'as'
-    cloneData.tags = []
-    cloneData.pin = 123
-
-    const test = jm.validate(cloneData, testSchema, { abortEarly: false })
+describe("Check the validation function", () => {
+  it("jm.validate() with no errors", () => {
+    const test = jm.validate(testData, testSchema);
+    expect(test).toBeNull();
+  });
+  it("jm.validate() with abortEarly: false", () => {
+    const cloneData = { ...testData };
+    cloneData.user = "as";
+    cloneData.pin = 123;
+    const test = jm.validate(cloneData, testSchema, { abortEarly: false });
     expect(test).toEqual({
-      user: 'user characters length must be greater than 5',
-      tags: 'tags must contain atleast 1 items',
-      pin: 'PIN must be 4 digit number',
-    })
-  })
-  it('jm.validate() with showAllErrors: true and abortEarly: false', () => {
-    const cloneData = { ...testData }
-    cloneData.user = 'as'
-    cloneData.tags = ''
-    cloneData.pin = 123
+      user: "user must atleast 5 characters",
+      pin: "PIN must be 4 digit number",
+    });
+  });
+  it("jm.validate() with showAllErrors: true and abortEarly: false", () => {
+    const cloneData = { ...testData };
+    cloneData.user = "as";
+    cloneData.tags = "";
+    cloneData.pin = 123;
 
     const test = jm.validate(cloneData, testSchema, {
       abortEarly: false,
       showAllErrors: true,
-    })
-    expect(test).toEqual({
-      user: ['user characters length must be greater than 5'],
-      tags: [
-        'tags must be an array',
-        'tags must contain atleast 1 items',
-        'All items must be match of the type',
-      ],
-      pin: ['PIN must be 4 digit number'],
-    })
-  })
-  it('jm.validate() single validation', () => {
-    expect(jm.validate('asd', jm.str())).toBeNull()
-    expect(jm.validate('', jm.str({ required: false }))).toBeNull()
-    expect(jm.validate(undefined, jm.str({ required: false }))).toBeNull()
+    });
+    expect(isJmArrayMsg(test.tags)).toBe(true);
+    expect(test).toMatchObject({
+      user: ["user must atleast 5 characters"],
+      tags: {},
+      pin: ["PIN must be 4 digit number"],
+    });
+  });
+
+  it("jm.validate({ strict: false }) ", () => {
+    testData.asd213 = 123;
+    const test = jm.validate(testData, testSchema, { strict: true });
+    expect(test).toMatchObject({ asd213: "asd213 is not part of schema" });
+  });
+  it("jm.validate() single validation", () => {
+    // Any
+    expect(jm.validate("asd", jm.any())).toBe(null);
+    expect(jm.validate(123, jm.any())).toBe(null);
+    expect(jm.validate([], jm.any())).toBe(null);
+    expect(jm.validate("", jm.any())).toBe(null);
+    expect(jm.validate(undefined, jm.any())).toBe(null);
+
+    //Array
+    expect(isJmArrayMsg(jm.validate(123, jm.array()))).toBeTruthy();
+    expect(jm.validate(arrayData, arraySchema)).toMatchObject({ 0: {} });
+
+    // String
+    expect(jm.validate("asd", jm.str())).toBeNull();
+    expect(jm.validate("", jm.str({ required: false }))).toBeNull();
+    expect(jm.validate(undefined, jm.str({ required: false }))).toBeNull();
     expect(
-      jm.validate('sircnujnuj@gmail.com', jm.str({ email: true })),
-    ).toBeNull()
+      jm.validate("darkcris2341@gmail.com", jm.str({ email: true }))
+    ).toBeNull();
+    expect(jm.validate(["asd"], jm.array({ items: jm.str() }))).toBeNull();
 
-    expect(jm.validate(123, jm.num())).toBeNull()
+    // Number
+    expect(jm.validate(12.023, jm.num({ float: true }))).toBeNull();
+    expect(!!jm.validate(12, jm.num({ float: true }))).toBeTruthy();
+    expect(jm.validate(123, jm.num())).toBeNull();
+    expect(jm.validate(123, jm.num({ integer: true }))).toBeNull();
+    expect(!!jm.validate(123.23, jm.num({ integer: true }))).toBeTruthy();
 
-    expect(jm.validate(123, jm.num({ integer: true }))).toBeNull()
+    // Object
+    expect(jm.validate({}, jm.obj({}))).toBeNull();
+    expect(jm.validate({ name: "" }, jm.obj({ name: jm.str() }))).toMatchObject(
+      { name: "name is required" }
+    );
+    expect(
+      jm.validate(undefined, jm.obj({ name: jm.str() }, { required: true }))
+    ).toBe("This is required");
 
-    expect(jm.validate(12.023, jm.num({ float: true }))).toBeNull()
-    expect(jm.validate(['asd'], jm.array({ items: jm.str() }))).toBeNull()
-    expect(jm.validate(true, jm.bool())).toBeNull()
-    expect(() => jm.validate('asdasd', jm.sameAs('password'))).toThrow()
-  })
-})
+    // Boolean
+    expect(jm.validate(true, jm.bool())).toBeNull();
+    expect(() => jm.validate("asdasd", jm.sameAs("password"))).toThrow();
+  });
+});
